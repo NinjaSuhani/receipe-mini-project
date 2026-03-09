e6d1b893c5a22b9af2350407a5f40a7944da67a0
NinjaSuhani <suhanifutane@gmail.com>
2026-03-09 15:56:33 +0530

    updated recipe UI

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 const container = document.getElementById("recipes-container");
 
 fetch("https://dummyjson.com/recipes")
-  .then(res => res.json())
-  .then(data => {
+.then(res => res.json())
+.then(data => {
 
     const recipes = data.recipes;
 
     recipes.forEach(recipe => {
 
-      const card = document.createElement("div");
-      card.classList.add("card");
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-      card.innerHTML = `
-        <img src="${recipe.image}" alt="">
-        <h3>${recipe.name}</h3>
-        <p>⭐ Rating: ${recipe.rating}</p>
-        <p>⏱️ Time: ${recipe.cookTimeMinutes} mins</p>
-      `;
+        card.innerHTML = `
+            <img src="${recipe.image}">
+            <div class="card-content">
+                <h3>${recipe.name}</h3>
+                <p>⭐ Rating: ${recipe.rating}</p>
+                <p>⏱️ Cook Time: ${recipe.cookTimeMinutes} mins</p>
+            </div>
+        `;
 
-      container.appendChild(card);
+        container.appendChild(card);
 
     });
 
-  })
-  .catch(error => {
-    console.log("Error fetching recipes:", error);
-  });
+});
